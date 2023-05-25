6fef0e55bb4919016bc3de793c885cc79a785d95
FernRicaurte <FernRicaurte2@gmail.com>
2023-05-25 12:14:18 -0400

    deleted unused code

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,8 @@ import './Navbar.css'
 
 import {Link} from 'react-router-dom'
 
-export default function NavBar({user, setUser}) {
+export default function Navbar({user, setUser}) {
 
-
-  function handleLogOut() {
-    setUser(null)
-  }
 
   return (
     <nav className='top-nav'>
