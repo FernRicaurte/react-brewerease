0d01be283540a5e86a06084a4218f95e66ede82d
FernRicaurte <FernRicaurte2@gmail.com>
2023-05-25 16:30:51 -0400

    attempting directory change for NavBar

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useState} from 'react'
 import {Routes, Route} from 'react-router-dom';
-import NavBar from '../../components/NavBar/NavBar';
+import NavBar from '../../components/NavBar/NavBar/NavBar';
 import Main from '../Main/Main';
 import BrewInfo from '../BrewInfo/BrewInfo'
 import StateBrewList from '../StateBrewList/StateBrewList';
